e29dc3f89aee0676005346029b8c24bb6180fc78
saracassandra7 <saracassandra7@gmail.com>
2022-11-11 17:26:03 +0100

    [add] esercise

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 const {createApp} = Vue;
 
 createApp({
-  
+  data(){
+    return{
+      emails: [],
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail'
+    }
+  },
+
+  methods:{
+    getApi(){
+      axios.get(this.apiUrl)
+      .then( result =>{
+        console.log(result.data);
+        this.emails.push(result.data.response)
+      })
+    }
+
+  },
+
+  mounted(){
+    for(let i = 0; i< 10; i++){
+      this.getApi();
+    }
+
+  }
 }).mount('#app')
