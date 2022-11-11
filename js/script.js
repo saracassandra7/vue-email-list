7d7abe39f7d7350b62816444d7eb8224de9da71a
saracassandra7 <saracassandra7@gmail.com>
2022-11-11 17:39:18 +0100

    [add] bonus (to fix)

--- js/script.js
+++ js/script.js
@@ -4,16 +4,19 @@ createApp({
   data(){
     return{
       emails: [],
-      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail'
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      isLoaded: false
     }
   },
 
   methods:{
     getApi(){
+      this.isLoaded= false;
       axios.get(this.apiUrl)
       .then( result =>{
         console.log(result.data);
-        this.emails.push(result.data.response)
+        this.emails.push(result.data.response);
+        this.isLoaded = true;
       })
     }
 
